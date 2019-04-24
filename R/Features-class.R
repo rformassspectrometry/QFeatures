769f8ae9770924105d9cd344e4868a056d3c0f45
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-04-24 22:56:45 +0200

    inital Features implementation

--- R/Features-class.R
+++ R/Features-class.R
@@ -1,23 +1,42 @@
 ##' @title Quantitative MS Features
 ##'
-##'
 ##' @aliases Features Features-class class:Features
 ##'
-##' @name Features
+##' @name Features 
 ##'
 ##' @description
 ##'
-##' Description comes here ...
+##' Conceptually, a `Features` object holds a set of `matrix` (or
+##' `array`) elements containing quantitative data. The number of
+##' columns (samples) are always the same across the matrices, but the
+##' number of rows (features) can vary. Each one of these matrices has
+##' a set of feature annotation (encoded as `DataFrame` objects), that
+##' have the same number of rows as the assay matrix their are
+##' associated to, and an arbitrary number of columns (feature
+##' variables). In addition, a `Features` object also uses a single
+##' `DataFrame` to annotate the samples (columns) represented in all
+##' the matrices. The largest assay matrix (the one with the highest
+##' number of features) is considered the main assay, from which the
+##' other ones are derived by aggregating/combining several rows into
+##' a single one.
 ##'
+##' A typical use case for such `Features` object if to represent
+##' quantitative proteomics or metabolomics data, where different
+##' assays represent quantitation data at the PSM (the main assay),
+##' peptide and protein level, and where peptide values are computed
+##' from the PSM data, and the protein-level data is calculated based
+##' on the peptide-level values.
 ##'
-##' `Features` objects can be created with the `Features` constructor that takes
-##' the following parameters:
+##' `Features` objects can be created with the `Features` constructor
+##' that takes the following parameters:
+##'
+##' TODO
 ##'
 ##' @details
 ##'
 ##' More details come here ...
 ##'
-##' @noRd
+##' @rdname Features-class
 ##'
 ##' @import S4Vectors
 ##' @importFrom SummarizedExperiment assays colData
@@ -30,14 +49,33 @@
 ##' @author Laurent Gatto
 ##'
 ##' @examples
-##' Features()
+##'
+##' m1 <- matrix(1:40, ncol = 4)
+##' m2 <- matrix(1:16, ncol = 4)
+##' colnames(m1) <- colnames(m2) <- paste0("S", 1:4)
+##' rownames(m1) <- letters[1:10]
+##' rownames(m2) <- letters[1:4]
+##'
+##' df1 <- DataFrame(Fa = 1:10, Fb = letters[1:10],
+##'                  row.names = letters[1:10])
+##' df2 <- DataFrame(row.names = letters[1:4])
+##'
+##' x <- new("Features",
+##'          assays = SimpleList(assay1 = m1, assay2 = m2),
+##'          featureData = SimpleList(assay1 = df1, assay2 = df2),
+##'          colData = DataFrame(row.names = colnames(m1)),
+##'          metadata = list(paste("Generated on", Sys.Date())))
+##' x
+NULL
+
 setClass("Features",
     representation(
         assays = "SimpleList",
-        featreData = "SimpleList",
+        featureData = "SimpleList",
         colData = "DataFrame",
-        metadata = "list"
-    ))
+        metadata = "list",
+        version = "character"),
+    prototype = prototype(version = "0.1"))
 
 setValidity("Features", .valid_Features)
 
@@ -56,31 +94,31 @@ setMethod("show", "Features",
 
 
 setMethod("names", "Features", function(x) names(x@assays))
+##' @exportMethod assays
 setMethod("assays", "Features", function(x, ...) x@assays)
 setMethod("dim", "Features", function(x) dim(x@assays[[.main_assay(x)]]))
 setMethod("length", "Features", function(x) length(x@assays))
 setMethod("isEmpty", "Features", function(x) length(x) == 0)
+##' exportMethod colData
 setMethod("colData", "Features", function(x) x@colData)
 setMethod("metadata", "Features", function(x, ...) x@metadata)
+##' @importFrom Biobase featureData
+setMethod("featureData", "Features", function(object) object@featureData)
+##' @exportMethod featureNames
 setMethod("featureNames", "Features",
-          function(object) rownames(object@fData))
+          function(object) rownames(object@featureData[[.main_assay(object)]]))
+setGeneric("featureVariables", function(object) standardGeneric("featureVariables"))
+##' @exportMethod featureVariables
+setMethod("featureVariables", "Features",
+          function(object) .featureVariables(object))
 
-featureVars <- function(object, assay = NULL) {
-    stopifnot(inherits(object, "Features"))
-    if (is.null(assay))
-        assay <- .main_assay(object)
-    if (is.character(assay)) {
-        assay <- assay[1]
-        stopifnot(assay %in% names(object))
-    }
-    names(object@fData[[assay]])
-}
-
+##' @exportMethod sampleNames
 setMethod("sampleNames", "Features",
           function(object) rownames(object@colData))
 setReplaceMethod("names", "Features",
     function(x, value) {
         names(x@assays) <- value
+        names(x@featureData) <- value
         x
     })
 
@@ -94,11 +132,4 @@ setReplaceMethod("metadata", "Features",
         x
     })
 
-## Features <- function(assays = SimpleList(),
-##                      fData = DataFrame(),
-##                      colData = DataFrame()) {
-##     new("Features",
-##         assays = assays,
-##         fData = fData,
-##         colData = colData)
-## }
+
--- R/Features-functions.R
+++ R/Features-functions.R
@@ -1,10 +1,77 @@
 .main_assay <- function(object)
     which.max(sapply(object@assays, nrow))
 
+.valid_Features_assay_dims <- function(object) {
+    l1 <- length(object@assays)
+    l2 <- length(object@featureData)
+    if (!identical(l1, l2))
+        stop("Different number of assays and feature data")        
+    all_dims <- sapply(object@assays, function(m) dim(m)[1:2])
+    if (any(is.na(all_dims)))
+        return(wmsg("all assays must be matrix-like objects ",
+                    "with at least 2 dimensions"))
+    if (!all(all_dims[2L, ] == all_dims[2L, 1L]))
+        stop("all assays must have the same number of cols")
+    NULL
+}
+
+.valid_Features_colData <- function(object) {
+    ## checking colData dimensions
+    p1 <- ncol(object@assays[[1]])
+    p2 <- nrow(object@colData)
+    if (p1 != p2)
+        stop("Number of samples and columns metadata don't match")
+    NULL
+}
+
+.valid_Features_featureData <- function(object) {
+    ## checking feature data dimension
+    n1 <- sapply(object@assays, nrow)
+    n2 <- sapply(object@featureData, nrow)
+    if (!all(n1 == n2))
+        stop("Number of features in assays and feature data don't match")
+
+    NULL
+}
+
+.valid_Features_rownames <- function(object) {
+    ## checking row names
+    rn1 <- lapply(object@assays, rownames)
+    rn2 <- lapply(object@featureData, rownames)        
+    if (!identical(rn1, rn2))
+        stop("Feature names in assay and feature data don't match")
+    NULL
+}
+
+.valid_Features_names <- function(object) {
+    nms1 <- names(object@assays)
+    nms2 <- names(object@featureData)
+    if (!identical(nms1, nms2))
+        stop("Names in assay and feature data don't match")
+}
+
+.valid_Features <- function(object) {
+    if (isEmpty(object))
+        return(NULL)
+    .valid_Features_assay_dims(object)
+    .valid_Features_colData(object)
+    .valid_Features_featureData(object)
+    .valid_Features_rownames(object)
+    .valid_Features_names(object)
+}
+
+
 .show_empty_Features <- function(object)
     cat("Empty", class(object), "object\n")
 
 .show_Features <- function(object) {
+    scat <- function(fmt, vals=character(), exdent=2, ...) {
+        vals <- ifelse(nzchar(vals), vals, "''")
+        lbls <- paste(S4Vectors:::selectSome(vals), collapse = " ")
+        txt <- sprintf(fmt, length(vals), lbls)
+        cat(strwrap(txt, exdent=exdent, ...), sep = "\n")
+    }
+    
     cat("class:", class(object), "\n")
     cat("dim:", dim(object), "\n")
 
@@ -15,66 +82,27 @@
     scat("metadata(%d): %s\n", expt)
 
     ## assays()
-    nms <- assayNames(object)
+    nms <- names(object)
     if (is.null(nms))
-        nms <- character(length(assays(object, withDimnames=FALSE)))
+        nms <- character(length(object@assays))
     scat("assays(%d): %s\n", nms)
     scat("Features(%d): %s\n", featureNames(object))
     scat("Samples(%d): %s\n", sampleNames(object))
 
     ## colData()
-    scat("Feature variables(%d): %s\n", names(colData(object)))
+    scat("Feature variables(%d): %s\n", featureVariables(object))
 }
 
 
-.valid_Features_assay_dims <- function(object) {
-    ## checking assays dimensions
-    all_dims <- sapply(object@assays, function(m) dim(m)[1:2])
-    if (any(is.na(all_dims)))
-        return(wmsg("all assays must be matrix-like objects ",
-                    "with 2 (or more?) dimensions"))
-    if (!all(all_dims[2L, ] == all_dims[2L, 1L]))
-        stop("all assays must have the same number of cols")
-
-    NULL
-}
-
-.valid_Features_colData <- function(object) {
-    ## checking colData dimensions
-    p1 <- ncol(object@assays[[1]])
-    p2 <- nrow(object@colData)
-    if (p1 != p2)
-        stop("Number of samples and columns metadata don't match")
-
-    NULL
-}
-
-.valid_Features_fData <- function(object) {
-    ## checking feature data dimension
-    n1 <- max(sapply(object@assays, nrow))
-    n2 <- nrow(object@fData)
-    if (n1 != n2)
-        stop("Number of features and feature metadata don't match")
-
-    NULL
-}
-
-.valid_Features_rownames <- function(object) {
-    ## checking row names
-    rn1 <- rownames(object@assays[[.main_assay(object)]])
-    rn2 <- rownames(object@fData)
-    if (!identical(rn1, rn2))
-        stop("Feature names don't match in assay and feature metadata")
-
-    NULL
-}
-
-
-.valid_Features <- function(object) {
+.featureVariables <- function(object, assay = NULL) {
+    stopifnot(inherits(object, "Features"))
     if (isEmpty(object))
-        return(NULL)
-    .valid_Features_assay_dims(object)
-    .valid_Features_colData(object)
-    .valid_Features_fData(object)
-    .valid_Features_rownames(object)
+        return(NA_character_)
+    if (is.null(assay))
+        return(unlist(unique(sapply(object@featureData, names))))
+    if (is.character(assay)) {
+        assay <- assay[1]
+        stopifnot(assay %in% names(object))
+    }
+    names(object@featureData[[assay]])
 }
