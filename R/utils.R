484d72e1e5b7af68bb8abb4be0deb1c96851f41c
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-04-29 20:26:05 +0200

    some todo notes

--- R/utils.R
+++ R/utils.R
@@ -78,3 +78,13 @@ setMethod("tidyFeatureData", c("Features", "numeric"),
           })
 
 
+## TODO: a programmatic version tidyDataFrame, that takes a vector of
+## the form c(fvar = "d", ....) and probably one that drops all List
+## columns.
+##
+## This begs the question whether it is necessary at all to have the
+## reduced versions, as one should be able to return to the source
+## FeatureSet, that contains all the feature variables.
+##
+## It might be useful to define some session-wide default rules?
+
