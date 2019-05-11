8eff8df5e0f7ba2bdc6d5279f0de837a21984a64
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-05-11 07:25:26 +0200

    add some unit tests

--- tests/testthat/test_Features.R
+++ tests/testthat/test_Features.R
@@ -1,6 +1,6 @@
 data(feat1)
 
-test_that("combineFeatures", {
+test_that("combineFeatures(fun = sum)", {
     feat1 <- combineFeatures(feat1, "psms", fcol = "Sequence", name = "peptides", fun = sum)
     expect_identical(names(feat1), c("psms", "peptides"))
 
@@ -30,5 +30,66 @@ test_that("combineFeatures", {
     alink <- feat1@assayLinks[[2]]
     expect_identical(alink@from, "psms")
     expect_identical(alink@fcol, "Sequence")
-    
+
+    hits1 <- Hits(from = c(rep(1, 3), rep(2, 4), rep(3, 3)),
+                  to = c(4:10, 1:3),
+                  names_to = c(rep("ELGNDAYK", 3),
+                               rep("IAEESNFPFIK", 4),
+                               rep("SYGFNAAR", 3)),                      
+                  nLnode = 3L,
+                  nRnode = 10L,
+                  names_from = paste0("PSM", c(4:10, 1:3)),
+                  sort.by.query = TRUE)
+    expect_identical(alink@hits, hits1)    
+})
+
+test_that("combineFeatures(fun = median)", {
+    feat1 <- combineFeatures(feat1, "psms", fcol = "Sequence", name = "peptides", fun = median)
+    expect_identical(names(feat1), c("psms", "peptides"))
+
+    ## checking quantiation data
+    assay1 <- matrix(c(median(1:3), median(4:6), median(7:10),
+                       median(11:13), median(14:16), median(17:20)),
+                     ncol = 2,
+                     dimnames = list(c("SYGFNAAR", "ELGNDAYK", "IAEESNFPFIK"),
+                                     c("S1", "S2")))
+    assay1 <- assay1[order(rownames(assay1)), ]
+    expect_identical(assay1, assay(feat1, "peptides"))
+
+    ## checking rowData
+    Sequence <- rownames(assay1)
+    Protein <- c("ProtA", "ProtB", "ProtA")
+    .n <- c(3, 4, 3)
+    names(.n) <- names(Protein) <- names(Sequence) <- Sequence
+
+    coldat1 <- DataFrame(Sequence = Sequence,
+                         Protein = Protein,
+                         .n = .n,                         
+                         row.names = rownames(assay1))
+    expect_equal(coldat1, rowData(feat1[["peptides"]]))
+
+
+    ## checking assayLinks
+    alink <- feat1@assayLinks[[2]]
+    expect_identical(alink@from, "psms")
+    expect_identical(alink@fcol, "Sequence")
+
+    hits1 <- Hits(from = c(rep(1, 3), rep(2, 4), rep(3, 3)),
+                  to = c(4:10, 1:3),
+                  names_to = c(rep("ELGNDAYK", 3),
+                               rep("IAEESNFPFIK", 4),
+                               rep("SYGFNAAR", 3)),                      
+                  nLnode = 3L,
+                  nRnode = 10L,
+                  names_from = paste0("PSM", c(4:10, 1:3)),
+                  sort.by.query = TRUE)
+    expect_identical(alink@hits, hits1)    
+})
+
+test_that("addAssay", {
+    data(feat1)
+    assay2 <- feat1[[1]]
+    feat1 <- addAssay(feat1, assay2, name = "psms2")
+    expect_identical(names(feat1), c("psms", "psms2"))
+    expect_identical(feat1[[1]], feat1[[2]])
 })
