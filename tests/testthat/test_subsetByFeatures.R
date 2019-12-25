bca01cb51aa9f02519e919f223d53439cf9f605b
Laurent Gatto <laurent.gatto@uclouvain.be>
2019-12-25 20:41:30 +0100

    test: subsetByFeatures unit test

---
+++ tests/testthat/test_subsetByFeatures.R
@@ -0,0 +1,17 @@
+data(feat1)
+
+test_that("subsetByFeatures", {
+    feat1 <- aggregateFeatures(feat1, 1, fcol = "Sequence", name = "peptides", fun = colMedians)
+    feat1 <- aggregateFeatures(feat1, 2, fcol = "Protein", name = "proteins", fun = colMedians)
+    res1 <- subsetByFeature(feat1, "ProtA")
+    res2 <- feat1["ProtA", ]
+    expect_equal(res1, res2)
+    expect_identical(lengths(feat1),
+                     c(nrow(feat1[[1]]),
+                       length(unique(rowData(feat1[[1]])[["Sequence"]])),
+                       length(unique(rowData(feat1[[1]])[["Protein"]]))))
+    expect_identical(sort(rownames(feat1[[2]])),
+                     sort(unique(rowData(feat1[[1]])[["Sequence"]])))
+    expect_identical(sort(rownames(feat1[[3]])),
+                     sort(unique(rowData(feat1[[1]])[["Protein"]])))
+})
