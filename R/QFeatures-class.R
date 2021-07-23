f10d11ec50ad6667a774ee7abe4eb60b3b23b227
Christophe Vanderaa <vanderaa.christophe@gmail.com>
2021-07-23 12:16:59 +0200

    refactor: small tweaking of the QFeatures plot (again)

--- R/QFeatures-class.R
+++ R/QFeatures-class.R
@@ -324,8 +324,11 @@ setMethod("show", "QFeatures",
 .offsetNodes <- function(coords) {
     lev <- coords[, 2]
     nlev <- length(unique(lev))
-    step <- max(coords[, 2]) / max(1, nlev - 1)
-    mar <- c(seq(1, 5, 2), seq(2, 6, 2)) / 10
+    ## Compute the step between levels, if only a single level the step=1
+    step <- ifelse(nlev > 1, max(coords[, 2]) / (nlev - 1), 1)
+    ## Custom margin between nodes so that the nodes are interleaved
+    ## (pattern repeats every 6 node)
+    mar <- c(1, 3, 5, 2, 4, 6) / 10
     for (i in unique(lev)) {
         sel <- lev == i
         ## Center x
