9ecd90d2fc3d9abbecdaea8f5ae51e7c4bb858e9
cvanderaa <vanderaa.christophe@gmail.com>
2024-04-10 21:10:58 +0200

    tests: testing .splitSCE()

--- tests/testthat/test_readQFeatures.R
+++ tests/testthat/test_readQFeatures.R
@@ -349,3 +349,22 @@ test_that("readSummarizedExperiment", {
         )
     expect_true(inherits(ft1, "SummarizedExperiment"))
 })
+
+test_that(".splitSE", {
+    m <- matrix(1:100, ncol = 10,
+                dimnames = list(paste0("row", 1:10),
+                                paste0("col", 1:10)))
+    se <- SummarizedExperiment(assay = m, 
+                               rowData = DataFrame(rowDataCol = 1:nrow(m)%%3),
+                               colData = DataFrame(colvar = 1:ncol(m)%%5))
+    ## Split by row
+    expect_identical(length(.splitSE(se, "rowDataCol")), 3L)
+    ## Split by col
+    expect_identical(length(.splitSE(se, "colvar")), 5L)
+    ## Error: variable not found
+    expect_error(.splitSE(se, "foo"),
+                 regexp = "not found")
+    ## Error: factor is too short
+    expect_error(.splitSE(se, factor(1:3)),
+                 regexp = "not compatible with dim")
+})
